2025701dc4056977ee7288d4dc27274017f3e23d
Antarpreet <pnarain82@gmail.com>
2023-08-03 19:00:28 +1000

    added routes

--- src/components/Skill.js
+++ src/components/Skill.js
@@ -1,37 +1,40 @@
 import React from "react";
+import Layout from "./Layout";
 
 function Skill() {
   return (
     <>
-      <div className="skill-section flex" id="skills">
-        <h2>My Skills</h2>
-        <div className="skillsdiv flex">
-          <div className="skill-list">
-            <div className="icon">
-              <i className="fa-brands fa-html5"></i>
+      <Layout>
+        <div className="skill-section flex" id="skills">
+          <h2>My Skills</h2>
+          <div className="skillsdiv flex">
+            <div className="skill-list">
+              <div className="icon">
+                <i className="fa-brands fa-html5"></i>
+              </div>
+              <div className="skill">HTML</div>
             </div>
-            <div className="skill">HTML</div>
-          </div>
-          <div className="skill-list">
-            <div className="icon">
-              <i className="fa-brands fa-html5"></i>
+            <div className="skill-list">
+              <div className="icon">
+                <i className="fa-brands fa-html5"></i>
+              </div>
+              <div className="skill">HTML</div>
             </div>
-            <div className="skill">HTML</div>
-          </div>
-          <div className="skill-list">
-            <div className="icon">
-              <i className="fa-brands fa-html5"></i>
+            <div className="skill-list">
+              <div className="icon">
+                <i className="fa-brands fa-html5"></i>
+              </div>
+              <div className="skill">HTML</div>
             </div>
-            <div className="skill">HTML</div>
-          </div>
-          <div className="skill-list">
-            <div className="icon">
-              <i className="fa-brands fa-html5"></i>
+            <div className="skill-list">
+              <div className="icon">
+                <i className="fa-brands fa-html5"></i>
+              </div>
+              <div className="skill">HTML</div>
             </div>
-            <div className="skill">HTML</div>
           </div>
         </div>
-      </div>
+      </Layout>
     </>
   );
 }
